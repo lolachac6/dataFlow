ecc1d3d845f47f820e505d8051b98cad7b70e94d
Lola <lola_chacon8@hotmail.com>
2025-07-16 13:09:47 +0200

    ng g s nodeServices

---
+++ src/app/services/node-services.service.ts
@@ -0,0 +1,70 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { Node, Edge } from 'ngx-vflow';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NodeServicesService {
+
+  nodes:  Node[] = [
+    {
+      id: '1',
+      point: ({ x: 100, y: 200 }),
+      type: 'html-template',
+      data: {default:'home', value:''}
+       
+    },
+   
+    {
+      id: '2',
+      point: ({ x: 500 ,y: 100 }) ,
+      type:'html-template',
+      data: {default:'contract', value:''}
+      
+    },
+     {
+      id: '3',
+      point: ({ x: 750 ,y: 200 }),
+      type: 'default',
+      text: 'Contratos',
+    },
+
+    
+  ];
+
+  edges:Edge[] =  [
+     {
+      id: '1 -> 2',
+      source: '1',
+      target: '2',
+      type: 'template',
+      
+      markers: {
+        end: {
+          type: 'arrow-closed',
+          width: 30,
+          height: 30,
+          color:'black',
+        },
+      },
+    },
+    {
+      id: '2 -> 3',
+      source: '2',
+      target: '3',
+      type: 'template',
+       markers: {
+        end: {
+          type: 'arrow-closed',
+          width: 30,
+          height: 30,
+          color:'black',
+        },
+      },  
+    }
+    
+  ];
+
+
+   
+}
