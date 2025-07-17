1588c44593ea2c5396ee68af8b3f9723a4f7b810
Lola <lola_chacon8@hotmail.com>
2025-07-17 12:34:10 +0200

    maquetado y style de componente contractList

--- src/app/components/node/contract-list-component/contract-list-component.component.ts
+++ src/app/components/node/contract-list-component/contract-list-component.component.ts
@@ -1,14 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { HandleComponent } from "ngx-vflow";
+import { Icontract } from '../../../interfaces/icontract.interface';
+import { ContractService } from '../../../services/contract.service';
+import { ModalComponent } from '../../static/modal/modal.component';
+
+
 
 @Component({
   selector: 'app-contract-list-component',
-  imports: [HandleComponent],
+  imports: [HandleComponent ,ModalComponent],
   templateUrl: './contract-list-component.component.html',
   styleUrl: './contract-list-component.component.css'
 })
 export class ContractListComponentComponent {
    @Input() ctx!: any;
    @Input() button!: any;
+    arrContracts: Icontract[] =[]; 
+    contractService = inject (ContractService)
+    activated:boolean = false;
+    
+
+
+   ngOnInit(){
+
+    this.arrContracts = this.contractService.contracts
+    
+
+   }
+
+   
+
+
+
 
 }
