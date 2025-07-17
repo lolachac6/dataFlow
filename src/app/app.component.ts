07384288f3e78daaf7a9d0c453885db551d6b6de
Lola <lola_chacon8@hotmail.com>
2025-07-17 14:47:57 +0200

    import de modelComponent en app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './components/static/header/header.component';
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ModalComponent } from "./components/static/modal/modal.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, DashboardComponent],
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
