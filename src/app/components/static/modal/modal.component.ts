6b42b2ed0833cd5af1a1645465ec7762a62b67d7
Lola <lola_chacon8@hotmail.com>
2025-07-18 02:24:33 +0200

    creación de validators en ReactiveForm

--- src/app/components/static/modal/modal.component.ts
+++ src/app/components/static/modal/modal.component.ts
@@ -1,18 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, effect, EventEmitter, inject, Input, Output } from '@angular/core';
 import { Icontract } from '../../../interfaces/icontract.interface';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SharedInformationService } from '../../../services/shared-information.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
 
-  @Input() contracts!:Icontract;
+ sharedService = inject(SharedInformationService)
+  router= inject(Router);
 
-  ngOnInit(){
+  contractForm:FormGroup
+  constructor(){
+    this.contractForm =new FormGroup({
+      idContract: new FormControl ("",[Validators.required, Validators.pattern(/^[A-Za-z0-9-]{6,}$/)]),
+      firstName: new FormControl ("", [Validators.required, Validators.pattern(/^[A-Za-z]{3,}$/)])
+      
+    },[])
+  }
+
+
+
+  cargarDatos(){
+    this.sharedService.setContract({id:this.contractForm.value.idContract,firstName:this.contractForm.value.firstName})
     
   }
+
+  checkControl(formControlName:string,validator:string){
+    return this.contractForm.get(formControlName)?.hasError(validator) 
+    && this.contractForm.get(formControlName)?.touched
+  }
+
+  
 
 }
