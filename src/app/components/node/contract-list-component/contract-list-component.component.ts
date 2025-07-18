6b21695ec39d87c8331d43ff6d4ae26ab55237c4
Lola <lola_chacon8@hotmail.com>
2025-07-18 02:24:09 +0200

    importación sharedInformationServices y ajustes

--- src/app/components/node/contract-list-component/contract-list-component.component.ts
+++ src/app/components/node/contract-list-component/contract-list-component.component.ts
@@ -1,34 +1,49 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, effect, inject, Input } from '@angular/core';
 import { HandleComponent } from "ngx-vflow";
 import { Icontract } from '../../../interfaces/icontract.interface';
 import { ContractService } from '../../../services/contract.service';
 import { ModalComponent } from '../../static/modal/modal.component';
+import { SharedInformationService } from '../../../services/shared-information.service';
+import { Ishared } from '../../../interfaces/ishared.interface';
 
 
 
 @Component({
   selector: 'app-contract-list-component',
-  imports: [HandleComponent ,ModalComponent],
+  imports: [HandleComponent],
   templateUrl: './contract-list-component.component.html',
   styleUrl: './contract-list-component.component.css'
 })
 export class ContractListComponentComponent {
-   @Input() ctx!: any;
-   @Input() button!: any;
-    arrContracts: Icontract[] =[]; 
-    contractService = inject (ContractService)
-    activated:boolean = false;
-    
+  @Input() ctx!: any;
+  @Input() button!: any;
 
+  contractService = inject(ContractService)
+  sharedData = inject(SharedInformationService);
 
-   ngOnInit(){
+  arrContracts: Icontract[] = [];
+  contracts!: Icontract[]
+  data!: Ishared;
+  
+  constructor() {
+    effect(() => {
 
+      this.data = this.sharedData.getContract();
+      if (this.data) {
+        const id = Object.values(this.data)[0]
+        const firstName = Object.values(this.data)[1];
+        this.contracts = this.arrContracts.filter(x => x.id === id && x.firstName === firstName)
+      }
+    })
+  }
+
+  ngOnInit() {
     this.arrContracts = this.contractService.contracts
-    
+  }
 
-   }
 
-   
+
+
 
 
 
