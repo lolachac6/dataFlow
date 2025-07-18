36a8203aace7d5347a09d477ca7af4e9629cfb7d
Lola <lola_chacon8@hotmail.com>
2025-07-18 19:18:14 +0200

    @input end-contractComponent

--- src/app/components/node/end-contract/end-contract.component.ts
+++ src/app/components/node/end-contract/end-contract.component.ts
@@ -1,7 +1,9 @@
-import { Component, effect, inject, Input } from '@angular/core';
+import { Component, effect, inject, Input, SimpleChanges } from '@angular/core';
 import { HandleComponent } from "ngx-vflow";
 import { SharedInformationService } from '../../../services/shared-information.service';
 import { Ishared } from '../../../interfaces/ishared.interface';
+import { Icontract } from '../../../interfaces/icontract.interface';
+import { ContractService } from '../../../services/contract.service';
 
 @Component({
   selector: 'app-end-contract',
@@ -11,15 +13,27 @@ import { Ishared } from '../../../interfaces/ishared.interface';
 })
 export class EndContractComponent {
   @Input() ctx!: any;
-  sharedData= inject(SharedInformationService)
-  data!:Ishared
+  @Input() sharedContract!: Ishared;
+  contractListService = inject(ContractService);
+    arrContract!:Icontract[]
+    dataContract!:Icontract[]
+    dataId!:any;
+    data!:Ishared
 
-   constructor() {
-    effect(() => {
+ ngOnChanges(changes: SimpleChanges): void {
+    if (!this.arrContract || this.arrContract.length === 0) {
+      this.arrContract = this.contractListService.contracts;
+    }
+    if (changes['sharedContract'] && this.sharedContract) {
+      const id = this.sharedContract.id;
+      const firstName = this.sharedContract.firstName;
 
-      this.data = this.sharedData.getContract();
-     
-    })
+      this.dataContract = this.arrContract.filter(
+        x => x.id === id && x.firstName === firstName
+      );
+      this.dataId = this.dataContract.length > 0 ? this.dataContract[0].id : undefined;
+    }
   }
-
 }
+
+
