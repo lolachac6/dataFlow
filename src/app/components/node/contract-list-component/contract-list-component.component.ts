da4e52c8cfc3f00a84156b355c3f10615fbfae47
Lola <lola_chacon8@hotmail.com>
2025-07-18 19:17:45 +0200

    creacion de @Input componente ContractList y modificacion html

--- src/app/components/node/contract-list-component/contract-list-component.component.ts
+++ src/app/components/node/contract-list-component/contract-list-component.component.ts
@@ -1,9 +1,5 @@
-import { Component, effect, inject, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HandleComponent } from "ngx-vflow";
-import { Icontract } from '../../../interfaces/icontract.interface';
-import { ContractService } from '../../../services/contract.service';
-import { ModalComponent } from '../../static/modal/modal.component';
-import { SharedInformationService } from '../../../services/shared-information.service';
 import { Ishared } from '../../../interfaces/ishared.interface';
 
 
@@ -17,35 +13,6 @@ import { Ishared } from '../../../interfaces/ishared.interface';
 export class ContractListComponentComponent {
   @Input() ctx!: any;
   @Input() button!: any;
-
-  contractService = inject(ContractService)
-  sharedData = inject(SharedInformationService);
-
-  arrContracts: Icontract[] = [];
-  contracts!: Icontract[]
-  data!: Ishared;
-  
-  constructor() {
-    effect(() => {
-
-      this.data = this.sharedData.getContract();
-      if (this.data) {
-        const id = Object.values(this.data)[0]
-        const firstName = Object.values(this.data)[1];
-        this.contracts = this.arrContracts.filter(x => x.id === id && x.firstName === firstName)
-      }
-    })
-  }
-
-  ngOnInit() {
-    this.arrContracts = this.contractService.contracts
-  }
-
-
-
-
-
-
-
+  @Input() shared!: Ishared;
 
 }
