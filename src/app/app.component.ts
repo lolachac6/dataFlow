ae58c9063aaaa341a408a5fc9b061f482e6935c5
Lola <lola_chacon8@hotmail.com>
2025-07-18 19:16:38 +0200

    import modalListComponent en appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './components/static/header/header.component';
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { ModalComponent } from "./components/static/modal/modal.component";
+import { ModalListContractsComponent } from "./components/static/modal-list-contracts/modal-list-contracts.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, DashboardComponent, ModalComponent],
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent, ModalComponent, ModalListContractsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
